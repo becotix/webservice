d579523e2adea124bd26324a9064e3187522d0f5
Calvin Hartwell <mail@hartwellsecurity.com>
2014-11-22 19:51:05 +0000

    Added Multiple Changes and updates to web service
    Multiple changes and additions to web service

--- webservice.py
+++ webservice.py
@@ -1,4 +1,6 @@
-from bottle import route, get, post, run, template
+from bottle import route, get, post, run, template, request
+from datetime import *
+from db import *
 
 @route("/")
 def index():
@@ -6,9 +8,17 @@ def index():
 
 @post("/register")
 def register():
-	return { "name": "register" }
-def register(name,email,address1,address2,city,region,postcode,cc_hash,password_hash):
-	return { "name": "register", "result":"successful"}
+	name = request.query['name']
+	email = request.query['email']
+	address1 = request.query['address_1']
+	address2 = request.query['address_2']
+	city = request.query['city']
+	region = request.query['region']
+	postcode = request.query['postcode']
+	cc_hash = request.query['cc_hash']
+	password = request.query['password_hash']
+	cursor.execute("INSERT INTO user VALUES (default,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(name,email,address1,address2,city,region,postcode,cc_hash,password))
+	return { "name": "register", "result":"successful", "uid":cursor.lastrowid}
 
 @get("/stop_info")
 def stop_info():
@@ -16,6 +26,24 @@ def stop_info():
 	beacon = {"bus_beacon":{"minor":"9000","major":"9000"}}
 	return {"name":"stop_info", 
 			"stops":[stop,stop,stop,stop], "bus_beacons":[beacon,beacon,beacon,beacon]}
+
+@get("/tickets")
+def tickets():
+	ticket = {"word":"cat", "date":datetime.datetime, "color":"#000"}
+	return {"name":"tickets",
+		"tickets":[ticket,ticket,ticket,ticket]}
+
+@get("/journeydata")
+def journeydata():
+	return {"name":"journeydata"}
+
+@get("/travelhistory")
+def travelhistory():
+	return {"name":"travelhistory"}
+
+@get("/personaldetails")
+def personaldetails():
+	return {"name":"personaldetails"}
 
 run(host='0.0.0.0', port=80)
 
