9f17d0d7a4791528331ea0498b067188dd73767f
Calvin Hartwell <mail@hartwellsecurity.com>
2014-11-22 13:37:26 +0000

    Added Multiple web service changes
    Added stop info request

--- webservice.py
+++ webservice.py
@@ -2,7 +2,7 @@ from bottle import route, get, post, run, template
 
 @route("/")
 def index():
-	return { "name": "Becotix Webservice 1.0" }
+	return { "name": "Becotix Webservice 1.0"}
 
 @post("/register")
 def register():
@@ -12,7 +12,10 @@ def register(name,email,address1,address2,city,region,postcode,cc_hash,password_
 
 @get("stop_info")
 def stop_info():
-	return {"name":"register"}
+	stop = {"name":"bus_stop1","major":"9000", "minor":"9000"}
+	beacon = {"minor":"9000","major":"9000"}
+	return {"name":"stop_info", 
+			"stops":{"stop":stop}, "bus_beacons":{"bus_beacon":beacon}}
 
 run(host='0.0.0.0', port=80)
 
