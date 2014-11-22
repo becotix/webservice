b41973615bde94a934557120d66746c4b4f0882f
Calvin Hartwell <mail@hartwellsecurity.com>
2014-11-22 13:56:20 +0000

    Updated web service + ip list
    Fixed stop_info request, added the db to the ip list

--- webservice.py
+++ webservice.py
@@ -10,12 +10,12 @@ def register():
 def register(name,email,address1,address2,city,region,postcode,cc_hash,password_hash):
 	return { "name": "register", "result":"successful"}
 
-@get("stop_info")
+@get("/stop_info")
 def stop_info():
-	stop = {"name":"bus_stop1","major":"9000", "minor":"9000"}
-	beacon = {"minor":"9000","major":"9000"}
+	stop = {"stop":{"name":"bus_stop1","major":"9000", "minor":"9000"}}
+	beacon = {"bus_beacon":{"minor":"9000","major":"9000"}}
 	return {"name":"stop_info", 
-			"stops":{"stop":stop}, "bus_beacons":{"bus_beacon":beacon}}
+			"stops":[stop,stop,stop,stop], "bus_beacons":[beacon,beacon,beacon,beacon]}
 
 run(host='0.0.0.0', port=80)
 
