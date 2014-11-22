c7d4e4cda157bcf52e5152ae75cb701761ba9ed3
Calvin Hartwell <mail@hartwellsecurity.com>
2014-11-22 13:10:14 +0000

    Added multiple web service changes
    Added changes for register support, stop info

--- webservice.py
+++ webservice.py
@@ -1,8 +1,18 @@
-from bottle import route, run, template
+from bottle import route, get, post, run, template
 
 @route("/")
-def index(name):
-	return template('<b>Becotix Webservice 1.0</b>!')
+def index():
+	return { "name": "Becotix Webservice 1.0" }
+
+@post("/register")
+def register():
+	return { "name": "register" }
+def register(name,email,address1,address2,city,region,postcode,cc_hash,password_hash):
+	return { "name": "register", "result":"successful"}
+
+@get("stop_info")
+def stop_info():
+	return {"name":"register"}
 
 run(host='0.0.0.0', port=80)
 
