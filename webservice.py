fc0d9ddc629d92a27afbcd69e3e3f8459cb79940
Calvin Hartwell <mail@hartwellsecurity.com>
2014-11-23 11:28:39 +0000

    Multiple API improvements, changes
    Multiple API improvements, changes

--- webservice.py
+++ webservice.py
@@ -1,11 +1,12 @@
 from bottle import route, get, post, run, template, request
 from datetime import *
 from db import *
+import json  
 
 @route("/")
 def index():
 	return { "name": "Becotix Webservice 1.0",
-			 "supported_methods":"register,stop_info,tickets,journeydata,travelhistory,personaldetails"}
+			 "supported_methods":"register,stop_info,tickets,journey_data,travelhistory,personaldetails"}
 
 @post("/register")
 def register():
@@ -24,37 +25,46 @@ def register():
 @get("/stop_info")
 def stop_info():
 	cursor.execute("SELECT * from stop")
-	stops = list(cursor.fetchall())
+	stops = [ dict(line) for line in [ zip([ column[0] for column in cursor.description ], 
+                 row) for row in cursor.fetchall() ] ]
 	cursor.execute("SELECT * from beacon")
-	beacons = list(cursor.fetchall())
+	beacons = [ dict(line) for line in [ zip([ column[0] for column in cursor.description ], 
+                 row) for row in cursor.fetchall() ] ]
 	return {"name":"stop_info", 
 			"stops":stops, "bus_beacons":beacons}
 
 @get("/tickets")
 def tickets():
-	cursor.execute("SELECT * from tickets")
-	tickets = list(cursor.fetchall())
-	return {"name":"tickets",
-		"tickets":tickets}
+	cursor.execute("SELECT id,colour,word from tickets")
+	tickets =  [ dict(line) for line in [ zip([ column[0] for column in cursor.description ], 
+                 row) for row in cursor.fetchall() ] ]
+	return {"name":"tickets","tickets":tickets}
 
-@get("/journeydata")
+@get("/journey_data")
 def journeydata():
-	cursor.execute("SELECT * from journey")
-	journeys = list(cursor.fetchall())
+	uid = request.query['uid']
+	startid = request.query['startid']
+	stopid = request.query['stopid']
+	starttime = request.query['starttime']
+	stoptime = request.query['stoptime']
+	journeys = "successful"
+	cursor.execute("INSERT INTO journey VALUES (default,%s,%s,%s,%s,%s,%s)",(uid,startid,stopid,starttime,stoptime,journeys))
 	return {"name":"journeys", 
-				"journeys":journeys}
+				"result":journeys}
 
 @get("/travelhistory")
 def travelhistory():
 	cursor.execute("SELECT * from journey")
-	travelhistory = list(cursor.fetchall())
+	travelhistory = [ dict(line) for line in [ zip([ column[0] for column in cursor.description ], 
+                 row) for row in cursor.fetchall() ] ]
 	return {"name":"travelhistory",
 			"travelhistory":travelhistory}
 
 @get("/personaldetails")
 def personaldetails():
 	cursor.execute("SELECT * from user")
-	userdata = list(cursor.fetchall())
+	userdata = [ dict(line) for line in [ zip([ column[0] for column in cursor.description ], 
+                 row) for row in cursor.fetchall() ] ]
 	return {"name":"personaldetails",
 			"personaldetails":userdata}
 
